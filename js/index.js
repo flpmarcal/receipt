5a3b8fd3fa1c197d1e7babc7110320ec25c8e3e4
Felipe Marçal <49991516+flpmarcal@users.noreply.github.com>
2023-11-13 18:25:09 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,184 +1,210 @@
-const btnAddItem = document.getElementById('btn-add-item')
+const createReceiptForm = document.getElementById('create-receipt-form')
+const addItemContainer = document.getElementById('add-item-container')
+const addItemBtn = document.getElementById('btn-add-item')
+const itemsContainer = document.getElementById('items-container')
+const createReceiptBtn = document.getElementById('create-receipt-btn')
+const extraInfoContainer = document.getElementById('extra-info-container')
 
-const inputName = document.querySelector('[data-value-name]')
-const inputDate = document.querySelector('[data-value-date]')
-const inputCustomMessage = document.querySelector('[data-value-message]')
+const inputCompany = document.querySelector('[data-company]')
+const inputDate = document.querySelector('[data-date]')
+const inputMessage = document.querySelector('[data-message]')
 
-const form = document.querySelector('[data-form]')
-const btnCreateReceipt = document.getElementById('btn-create-receipt')
+const localItemsString = localStorage.getItem('items-list')
+const localExtraInfosString = localStorage.getItem('extra-info')
 
-const itemsData = localStorage.getItem('items-data');
-const items = JSON.parse(itemsData)
+const localExtraInfos = JSON.parse(localExtraInfosString)
+let localItems = JSON.parse(localItemsString)
 
-const receiptInfoData = localStorage.getItem('receipt-info-data')
-const receiptInfo = JSON.parse(receiptInfoData)
+let removeButtons = []
 
-const sectionItems = document.getElementById('items-section')
-const newItem = `
-    <section class="item" id="section-item" data-item>
-            <input type="text" class="input-item" placeholder="Item" value="" data-value-title />
-            <input type="number" class="input-item" placeholder="Amount" value="" pattern="\d*" data-value-amount />
-            <span class="multiplier">X</span>
-            <input type="number" class="input-item" placeholder="Value" value="" pattern="\d*" data-value-value />
-            <div class="button secondary" data-btn-remove><img src="img/ic-delete.png" width="20px"/></div>
-    </section>
-`
-
-if (items) {
-    displayLocalData()
-    monitorRemoveButtons()
-}
+displayLocalData()
 
 function displayLocalData() {
-    items.forEach((item) => {
-        const newItemLocal = `
-            <section class="item" id="section-item" data-item>
-                <input type="text" class="input-item" placeholder="Item" value="${item.title}" data-value-title />
-                <input type="number" class="input-item" placeholder="Amount" value="${item.amount}" data-value-amount />
-                <span class="multiplier">X</span>
-                <input type="number" class="input-item" placeholder="Value" value="${item.value}" data-value-value />
-                <div class="button secondary" data-btn-remove data-local><img src="img/ic-delete.png" width="20px"/></div>
-            </section>
-        `
-        sectionItems.insertAdjacentHTML('beforeend', newItemLocal)
-    })
+    if (localItemsString) {
+        localItems.forEach((localItem) => {
+            localItem.source = 'local'
+            itemsContainer.insertAdjacentHTML('beforeend', newlItemElement(localItem))
+        })
+    }
 
-    inputName.value = receiptInfo[0]
-    inputDate.value = receiptInfo[1]
-    inputCustomMessage.value = receiptInfo[2]
+    if (localExtraInfosString) {
+        inputCompany.value = localExtraInfos.company
+        inputMessage.value = localExtraInfos.message
+    }
 }
 
-// Escuta o evento de focus no 'inputDate'.
-inputDate.addEventListener('focus', () => {
-    // Muda o type do 'inputDate' para 'date'.
-    inputDate.type = 'date'
-})
+function newlItemElement(item) {
+    const newItem = `
+    <section class="item-container" data-item>
+        <input name="itemTitle" type="text" class="input-item" placeholder="Item" value="${item.title}" data-source="${item.source}" data-title />
+        <input name="itemAmount" type="text" class="input-item" placeholder="Amount" value="${item.amount}" data-source="${item.source}" data-amount />
+        <span class="multiplier">X</span>
+        <input name="itemPrice" type="text" class="input-item" placeholder="0.00" value="${item.price}" autocomplete="off" maxlength="10" data-source="${item.source}" data-price />
+        <div class="button secondary btn-delete" data-source="${item.source}" data-btn-source="${item.source}"></div>
+    </section>
+    `
+    return newItem
+}
 
-// Escuta o evento de blur no 'inputDate'.
-inputDate.addEventListener('blur', () => {
-    // Muda o type do 'inputDate' para 'text'.
-    inputDate.type = 'text'
-})
-
-// Escuta o evento de 'click' no 'btnAddItem'.
-btnAddItem.addEventListener('click', (addItemEvent) => {
-
-    // Executa a função 'addItemOnList' e 'monitorRemoveButtons'.
-    addItemOnList()
-    monitorRemoveButtons()
-    
-    // Dá o focus no próximo input adicionado.
-    const lastItem = sectionItems.lastElementChild
-    const firstInputLastItem = lastItem.querySelector('[data-value-title]')
+addItemBtn.addEventListener('click', () => {
+    const emptyItem = {
+        source: 'ui',
+        title: '',
+        amount: '',
+        price: ''
+    }
+    itemsContainer.insertAdjacentHTML('beforeend', newlItemElement(emptyItem))
+    const lastItem = itemsContainer.lastElementChild
+    const firstInputLastItem = lastItem.querySelector('[data-title]')
     firstInputLastItem.focus()
 })
 
-// Escuta o evento de 'click' no 'btnCreateReceipt'.
-form.addEventListener('submit', (createReceiptEvent) => {
-    // Previne o comportamento padrão do evento 'createReceiptEvent'.
-    createReceiptEvent.preventDefault()
-    // Limpa o que estava no 'localStorage'.
-    localStorage.clear()
-    // Roda a função 'dataCapture' para preencher o localStorage com os novos dados preenchidos.
-    dataCapture()
-    // Redireciona para a tela de criação do recibo.
-    window.location.href = './receipt.html';
+
+
+itemsContainer.addEventListener('click', (e) => {
+    const target = e.target
+    const itemContainer = target.parentNode
+    if (target.matches('[data-btn-source="local"]')) {
+        const itemTitle = itemContainer.querySelector('[data-title]').value
+        const itemFoundIndex = localItems.findIndex((item) => item.title === itemTitle)
+        localItems.splice(itemFoundIndex, 1)
+        const newItemsListString = JSON.stringify(localItems)
+        localStorage.setItem('items-list', newItemsListString)
+        itemContainer.remove()
+    } else if (target.matches('[data-btn-source="ui"]')) {
+        itemContainer.remove()
+    } else if (target.matches('[data-price]')) {
+        target.addEventListener('blur', (e) => {
+            const inputPrice = e.target
+            formatPrice(inputPrice)
+        })
+    }
 })
 
-// Cria a função que monitora os novos bot!oes de remover criados.
-function monitorRemoveButtons() {
-    let removeButtons = document.querySelectorAll('[data-btn-remove]')
-    listenRemoveButtons(removeButtons)
+itemsContainer.addEventListener('keydown', (e) => {
+    if (e.target.matches('[data-price]') && e.key === 'Tab') {
+        formatPrice(e.target)
+    }
+})
+
+function formatPrice(priceField) {
+    const plainValue = priceField.value
+    function hasOneDigitAfterDecimal(num) {
+        var numStr = num.toString();
+        var decimalIndex = numStr.indexOf('.');
+        if (decimalIndex === -1) {
+            return false;
+        }
+        var digitsAfterDecimal = numStr.length - decimalIndex - 1;
+        return digitsAfterDecimal === 1;
+    }
+    const checkFloating = priceField.value.includes('.')
+    const checkFloatingAmount = hasOneDigitAfterDecimal(priceField.value)
+    if (checkFloating && checkFloatingAmount) {
+        priceField.value = plainValue + '0'
+    } else if (!checkFloating && plainValue != 0) {
+        priceField.value = plainValue + '.00'
+    }
 }
 
-// Cria a função 'addItemOnList'.
-function addItemOnList() {
-    // Adiciona ao fim do node de 'sectionItems' o conteudo da const 'newItem'.
-    sectionItems.insertAdjacentHTML('beforeend', newItem)
-}
+createReceiptForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const itemsContainers = itemsContainer.querySelectorAll('[data-item]')
+    saveItems(itemsContainers)
+})
 
-function listenRemoveButtons(removeButtons) {
-    // Para cada 'btnRemoveItem' na tela.
-    removeButtons.forEach((removeButton) => {
-        // Escuta o evento de 'click'.
-        removeButton.addEventListener('click', function() {
-            // Cria a const 'itemFromLocalDataCheck' que retorna true se o botão clicado é de um item gerado pelo 'localStorage'.
-            const itemFromLocalDataCheck = removeButton.hasAttribute('data-local')
-            
-            // Se o botão clicado for de um item vindo do local storage.
-            if (itemFromLocalDataCheck) {
-                // Captura o valor do titulo daquele item.
-                const itemToBeRemoved = removeButton.parentNode
-                const itemTitleInput = itemToBeRemoved.querySelector('[data-value-title]')
-                const itemTitle = itemTitleInput.value
+function saveItems(itemsToBeSaved) {
+    localStorage.clear()
 
-                // Busca o index daquele item na array do local storage.
-                const itemFound = items.findIndex((item) => item.title === itemTitle)
+    let isThereInvalidFields = false
 
-                // Remove aquele item do array 'items'.
-                items.splice(itemFound, 1)
+    const titleInputs = itemsContainer.querySelectorAll('[data-title]')
+    const amountInputs = itemsContainer.querySelectorAll('[data-amount]')
+    const priceInputs = itemsContainer.querySelectorAll('[data-price]')
 
-                // Atualiza o local storage com o novo array 'itens'.
-                localStorage.setItem('items-data', JSON.stringify(items));
+    for (const titleInput of titleInputs) {
+        if (!verifyTitles(titleInput)) {
+            isThereInvalidFields = true
+        }
+    }
 
-                // Remove o node daquele item da tela.
-                itemToBeRemoved.remove();
-            } else {
-                // Cria a const 'itemToBeRemoved' que armazena o node pai do botão de remover clicado.
-                const itemToBeRemoved = removeButton.parentNode
-                // Remove o 'itemToBeRemoved', node pai do botão de remover.
-                itemToBeRemoved.remove()
-            }
-        })
+    for (const amountInput of amountInputs) {
+        if (!verifyNumbers(amountInput)) {
+            isThereInvalidFields = true
+        }
+    }
+
+    for (const priceInput of priceInputs) {
+        if (!verifyNumbers(priceInput)) {
+            isThereInvalidFields = true
+        }
+    }
+
+    if (isThereInvalidFields) {
+        return
+    }
+
+    let itemsList = []
+
+    itemsToBeSaved.forEach((itemToBeSaved) => {
+        const titleInput = itemToBeSaved.querySelector('[data-title]')
+        const amountInput = itemToBeSaved.querySelector('[data-amount]')
+        const priceInput = itemToBeSaved.querySelector('[data-price]')
+        
+        const item = {
+            title: titleInput.value,
+            amount: amountInput.value,
+            price: priceInput.value
+        }
+
+        itemsList.push(item)
     })
+
+    const extraInfo = {
+      company: inputCompany.value,
+      date: inputDate.value,
+      message: inputMessage.value
+    }
+
+    const extraInfoString = JSON.stringify(extraInfo)
+    const itemsListString = JSON.stringify(itemsList)
+
+    localStorage.setItem('items-list', itemsListString)
+    localStorage.setItem('extra-info', extraInfoString)
+
+    window.location.href = './receipt.html';
 }
 
-// Cria a função 'dataCapture'.
-function dataCapture() {
-
-    // Cria o array 'tempItemList' para armazenar as infos do item daquela iteração.
-    let tempItemList = []
-
-    // Cria a const 'sectionItem' que armazena todos os nodes dos itens preenchidos.
-    const sectionItem = document.querySelectorAll('[data-item]')
-
-    // Para cada 'nodeItem' dentro do 'sectionItem'.
-    sectionItem.forEach((nodeItem) => {
-
-        // Captura os inputs no HTML que armazenam titulo, amount e value do item.
-        const itemTitle = nodeItem.querySelector('[data-value-title]').value
-        const itemAmount = nodeItem.querySelector('[data-value-amount]').value
-        const itemValue = nodeItem.querySelector('[data-value-value]').value
-
-        // Atualiza o array 'tempItemList' com as infos dos item daquela iteração.
-        tempItemList.push({
-            title: itemTitle,
-            amount: itemAmount,
-            value: itemValue
-        })
-    })
-
-    // Cria o array 'itemsList' e popula com os itens da array 'tempItemList'.
-    const itemsList = [...tempItemList]
-
-    // Cria as consts que acessam os valores do que foi digitado nos inputs de informações extras.
-    const nameValue = inputName.value
-    const dateValue = inputDate.value
-    const customMessageValue = inputCustomMessage.value
-
-    // Cria a const que armazena o objeto com as infos adicionais do recibo.
-    const extraInfos = [nameValue, dateValue, customMessageValue]
-
-    // Executa a função 'saveItems' passando 'itemsList' e 'extraInfos' como parametros.
-    saveItems(itemsList, extraInfos)
+function generateCurrentDate() {
+    const currentDate = new Date()
+    const day = String(currentDate.getDate()).padStart(2, '0')
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0')
+    const year = currentDate.getFullYear()
+    const formattedDate = `${month}/${day}/${year}`
+    return formattedDate
 }
 
+inputDate.value = generateCurrentDate()
 
-// Cria a função 'saveItems' que espera os 'itemsToBeSaved' e 'extraInfosToBeSaved' como parâmetros.
-function saveItems(itemsToBeSaved, extraInfosToBeSaved) {
-    const itemsData = JSON.stringify(itemsToBeSaved)
-    localStorage.setItem('items-data', itemsData)
+function verifyTitles(titleField) {
+    if (titleField.value == '') {
+        titleField.classList.add('item-field-invalid')
+        titleField.addEventListener('input', e => e.target.classList.remove('item-field-invalid'))
+        return false
+    } else {
+        return true
+    }
+}
 
-    const infosData = JSON.stringify(extraInfosToBeSaved)
-    localStorage.setItem('receipt-info-data', infosData) 
+function verifyNumbers(numberField) {
+    const regex = /^[0-9.,\/]+$/
+    const fieldTest = regex.test(numberField.value)
+
+    if (!fieldTest || numberField.value == '') {
+        numberField.classList.add('item-field-invalid')
+        numberField.addEventListener('input', e => e.target.classList.remove('item-field-invalid'))
+        return false
+    } else {
+        return true
+    }
 }
