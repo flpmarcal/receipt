ca91af11a8fef1c6908403a3ea29cae67f3d7a3b
Felipe Marçal <49991516+flpmarcal@users.noreply.github.com>
2023-11-14 18:27:39 -0300

    Delete js/validations.js

--- js/validations.js
+++
@@ -1,8 +0,0 @@
-// let inputFields = []
-
-// function getInputfields(newItemSection) {
-//     const existingInputFields = document.querySelectorAll('[required]')
-//     const newItemSection = newItemSection.querySelectorAll('[required]')
-
-//     console.log
-// }
